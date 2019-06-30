43cc964ba1d3df023bff18212e8611e95dfd633e
Armando Gomez Chapa <armando.gomez.danes@gmail.com>
2019-06-30 16:04:19 -0500

    complete firebase config cosntants

--- src/core/utils/config.ts
+++ src/core/utils/config.ts
@@ -1,4 +1,8 @@
 export const firebaseConfig =  {
     apiKey: "AIzaSyAlW_DGQx1NMV-ao_we5aknl551-xg3uWo",
-    authDomain: "uikittenauth.firebaseapp.com"
+    authDomain: "uikittenauth.firebaseapp.com",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
